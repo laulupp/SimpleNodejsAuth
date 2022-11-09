445341b7777cf3053519584918e8b714b495a337
laulupp <lupp.laurentiu@gmail.com>
2022-11-09 21:05:10 +0200

    build: update package.json

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ let app = express();
 
 let port = 80;
 
+app.get("/", (req, res) => {
+  res.render("views/index.ejs");
+});
+
 app.listen(port, () => {
   console.log("Started on port " + port);
 });
