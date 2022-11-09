ce1a9a06f9bd4e08d16e3838ac779c16a30ab462
laulupp <lupp.laurentiu@gmail.com>
2022-11-09 22:00:57 +0200

    update app.js

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express();
 const port = 80;
 
 app.set("view-engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("port", process.env.PORT || port);
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.listen(port, () => {
-  console.log("Started on port " + port);
+app.listen(app.get("port"), () => {
+  console.log("Started on port " + app.get("port"));
 });
