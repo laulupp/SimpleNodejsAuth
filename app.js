0b75d78c33697da6f9e7ae173207dad6c786ff6b
laulupp <lupp.laurentiu@gmail.com>
2022-11-16 22:32:26 +0200

    update .gitignore, app.js, package.json and routes/index.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const routerIndex = require("./routes/index");
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true} );
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 80);
 
+app.use(express.static(__dirname + '/public'));
 app.use("/", routerIndex);
 
 app.listen(app.get("port"), () => {
